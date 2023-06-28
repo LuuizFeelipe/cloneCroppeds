7a9255da9378074cb89821015cece69e1ac8d0cc
LuuizFeelipe <110106741+LuuizFeelipe@users.noreply.github.com>
2023-06-28 10:17:58 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 ScrollReveal({ reset: true });
 ScrollReveal().reveal('.banners__home, .banners__vantagem, .grid-flex-banners, .newsletter, .home__banner, .footer', {
-    duration: 1000,
+    duration: 900,
 });
